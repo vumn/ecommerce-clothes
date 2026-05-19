03e42f6534885036208e35de02c7fc34536ff24e
vunm <vunongdann@gmail.com>
2026-05-19 10:04:03 +0700

    create Search bar UI

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 // import React from 'react'
 import {assets} from '../assets/assets'
 import { Link, NavLink } from 'react-router-dom'
-import { useState } from 'react'
+import { useContext, useState } from 'react'
+import { ShopContext } from '../context/ShopContext';
 const Navbar = () => {
     
     const [visible, setVisible] = useState(false);
-  
+    
+    const {setShowSearch} = useContext(ShopContext);
+
     return (
     <div className='flex items-center justify-between py-5 font-medium'>
         <Link to='/'><img src={assets.logo} className='w-36' alt='' /></Link>
@@ -30,7 +33,7 @@ const Navbar = () => {
         </ul>
 
         <div className='flex items-center gap-6'>
-            <img src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
+            <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
             <div className='group relative'>
                 <img className='w-5 cursor-pointer' src={assets.profile_icon} />
                 <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { products } from "../assets/assets";
+// import { useState } from "react";
 
 export const ShopContext = createContext();
 
@@ -7,9 +8,14 @@ const ShopContextProvider = (props) => {
 
     const currency = '$';
     const delivery_fee = 10;
+    const [search, setSearch] = useState([]);
+    const [showSearch, setShowSearch] = useState(false);
+
+
 
     const value = {
-        products, currency, delivery_fee
+        products, currency, delivery_fee,
+        search, setSearch, showSearch, setShowSearch
     }
     return (
         <ShopContext.Provider value={value}>
