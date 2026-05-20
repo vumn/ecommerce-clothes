9891239373e298334e49e2660a38588c007ee448
vunm <vunongdann@gmail.com>
2026-05-20 10:08:58 +0700

    create logic for ADD TO CART button

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
+import { toast } from "react-toastify";
 // import { useState } from "react";
 
 export const ShopContext = createContext();
@@ -10,12 +11,43 @@ const ShopContextProvider = (props) => {
     const delivery_fee = 10;
     const [search, setSearch] = useState([]);
     const [showSearch, setShowSearch] = useState(false);
+    const [cartItems, setCartItems] = useState({});
 
+    const addToCart = async (itemId, size) => {
+        if (!size) {
+            toast.error('Please Select Product Size');
+            return;
+        }
+
+        let cartData = structuredClone(cartItems);
+
+        if (cartData[itemId]) {
+            // Increase the amount if exist
+            if (cartData[itemId][size]) {
+                cartData[itemId][size] += 1;
+            }
+            // If not exist in cart, put item in cart
+            else {
+                cartData[itemId][size] = 1;
+            }
+        }
+        else {
+            cartData[itemId] = {};
+            cartData[itemId][size] = 1;
+        }
+        setCartItems(cartData);
+    }
+
+    useEffect(() => {
+        console.log(cartItems);
+        
+    }, [cartItems])
 
 
     const value = {
         products, currency, delivery_fee,
-        search, setSearch, showSearch, setShowSearch
+        search, setSearch, showSearch, setShowSearch,
+        cartItems, addToCart
     }
     return (
         <ShopContext.Provider value={value}>
