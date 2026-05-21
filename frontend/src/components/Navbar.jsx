0463320e02070abfbe6fa298185a3dbcc0ccd3a0
vunm <vunongdann@gmail.com>
2026-05-21 16:15:45 +0700

    create UI for login/signup form

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
         <div className='flex items-center gap-6'>
             <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
             <div className='group relative'>
-                <img className='w-5 cursor-pointer' src={assets.profile_icon} />
+                <Link to='/login'><img className='w-5 cursor-pointer' src={assets.profile_icon} /></Link>
                 <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
                     <div className='flec flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
                         <p className='cursor-pointer hover:text-black'>My Profile</p>
