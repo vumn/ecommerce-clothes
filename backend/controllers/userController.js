675e738a8ea5b1c23be1daac7549c816c108a574
vunm <vunongdann@gmail.com>
2026-05-24 09:38:04 +0700

    create adminLogin

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -73,7 +73,19 @@ const registerUser = async (req, res) => {
 
 // Route for admin login
 const adminLogin = async (req, res) => {
-
+    try {
+        const {email, password} = req.body;
+        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+            const token = jwt.sign(email+password, process.env.JWT_SECRET);
+            res.json({success:true, token});
+        }
+        else {
+            res.json({success:false, message:"Invalid email or password"})
+        }
+    } catch (error) {
+        console.error("Error in adminLogin", error);
+        res.json({succes:false, message:error.message})
+    }
 }
 
 export {loginUser, registerUser, adminLogin}
