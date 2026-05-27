924f6016329797c576f0551a317f32f21be50f6c
vunm <vunongdann@gmail.com>
2026-05-27 11:20:29 +0700

    finish sidebar and navbar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
+import {Routes, Route} from 'react-router-dom'
+import Add from './pages/Add'
+import List from './pages/List'
+import Orders from './pages/Orders'
 
 const App = () => {
   return (
@@ -10,9 +14,16 @@ const App = () => {
         <hr />
         <div className='flex w-full'>
           <Sidebar />
+          <div className='w-[70%] mx-auto ml-[max(5vw,25p)] my-8 text-gray-600 text-base'>
+            <Routes>
+              <Route path='/add' element={<Add />} />
+              <Route path='/list' element={<List />} />
+              <Route path='/orders' element={<Orders />} />
+            </Routes>
+          </div>
         </div>
       </>
-    <div/>
+    </div>
   )
 }
 
