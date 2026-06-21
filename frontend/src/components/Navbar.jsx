ba21e06eb0600550d17acfe3c744da083a972d00
vunm <vunongdann@gmail.com>
2026-06-21 14:57:53 +0700

    orders dropdown

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -50,7 +50,7 @@ const Navbar = () => {
                 <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
                     <div className='flec flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
                         <p className='cursor-pointer hover:text-black'>My Profile</p>
-                        <p className='cursor-pointer hover:text-black'>Orders</p>
+                        <p onClick={()=> navigate('/orders')} className='cursor-pointer hover:text-black'>Orders</p>
                         <p onClick={logout} className='cursor-pointer hover:text-black'>Logout</p>
                     </div>
                 </div>}
