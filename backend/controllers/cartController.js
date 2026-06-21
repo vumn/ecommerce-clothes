3c6ad867c0189bcb0552cbb3088322a65be1f561
vunm <vunongdann@gmail.com>
2026-06-21 15:20:20 +0700

    create logic for user's cart

---
+++ backend/controllers/cartController.js
@@ -0,0 +1,69 @@
+import User from "../models/User.js";
+
+// add product to user cart
+const addToCart = async (req, res) => {
+    try {
+        
+        const {userId, itemId, size} = req.body;
+        
+        const userData = await User.findById(userId);
+
+        let cartData = await userData.cartData;
+
+        if (cartData[itemId]) {
+            if (cartData[itemId][size]) {
+                cartData[itemId][size] += 1;
+            }
+            else {
+                cartData[itemId][size] = 1;
+            }
+        }
+        else {
+            cartData[itemId] = {};
+            cartData[itemId][size] = 1;
+
+        }
+        await User.findByIdAndUpdate(userId, {cartData});
+
+        res.json({success: true, message: "Added to Cart"});
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message: error.message});
+    }
+}
+
+// update user cart 
+const updateCart = async (req, res) => {
+    try {
+        const {userId, itemId, size, quantity} = req.body;
+        const userData = await User.findById(userId);
+
+        let cartData = await userData.cartData;
+
+        cartData[itemId][size] = quantity;
+
+        await User.findByIdAndUpdate(userId, {cartData});
+        res.json({success: true, message: "Cart updated"});
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message: error.message});
+    }
+}
+
+// get user cart data
+const getUserCart = async (req, res) => {
+    try {
+        const {userId} = req.body;
+        const userData = await User.findById(userId);
+
+        let cartData = await userData.cartData;
+        res.json({success:true, cartData});
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message: error.message});
+    }
+}
+
+export {addToCart, updateCart, getUserCart}
