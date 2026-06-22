04cc6de1525e7370159bc166193d92792fc11ae8
vunm <vunongdann@gmail.com>
2026-06-22 20:16:46 +0700

    fetch user data for cart

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -41,6 +41,17 @@ const ShopContextProvider = (props) => {
             cartData[itemId][size] = 1;
         }
         setCartItems(cartData);
+
+        if (token) {
+            try {
+                await axios.post(backendUrl + '/api/cart/add', {itemId, size}, {headers: {token}});
+
+            } catch (error) {
+                console.log(error);
+                toast.error(error.message);
+                
+            }
+        }
     }
 
     const getCartAmount = () => {
@@ -87,6 +98,17 @@ const ShopContextProvider = (props) => {
         cartData[itemId][size] = quantity;
 
         setCartItems(cartData);
+
+        if (token) {
+            try {
+                await axios.post(backendUrl + '/api/cart/update', {itemId, size, quantity}, {headers: {token}});
+
+            } catch (error) {
+                console.log(error);
+                toast.error(error.message);
+                
+            }
+        }
     }
 
     const getProductsData = async () => {
@@ -106,13 +128,27 @@ const ShopContextProvider = (props) => {
         }
     }
 
+    const getUserCart = async(token) => {
+        try {
+            const response = await axios.post(backendUrl + '/api/cart/get', {}, {headers: {token}});
+            if (response.data.success) {
+                setCartItems(response.data.cartData);
+            }
+
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message);
+        }
+    }
+
     useEffect(() => {
-        getProductsData()
+        getProductsData();
     }, [])
 
     useEffect(() => {
         if (!token && localStorage.getItem('token')) {
-            setToken(localStorage.getItem('token'))
+            setToken(localStorage.getItem('token'));
+            getUserCart(localStorage.getItem('token'));
         }
     }, [])
     const value = {
