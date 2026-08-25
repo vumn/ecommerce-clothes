b6449462d0c2ac384498c3c808a3ce139c69d2f5
vunm <vunongdann@gmail.com>
2026-08-25 23:05:50 +0700

    minor change in user's order list

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -22,10 +22,10 @@ const Orders = () => {
         let allOrdersItem = []
         response.data.orders.map((order) => {
           order.items.map((item) => {
-            item['status'] == order.status
-            item['payment'] == order.payment
-            item['paymentMethod'] == order.paymentMethod
-            item['date'] == order.date
+            item['status'] = order.status
+            item['payment'] = order.payment
+            item['paymentMethod'] = order.paymentMethod
+            item['date'] = order.date
             allOrdersItem.push(item)
           })
         })
@@ -59,21 +59,24 @@ const Orders = () => {
                 <div>
                   <p className='sm:text-base font-medium'>{item.name}</p>
                   <div className='flex items-center gap-3 mt-2 text-base text-gray-700'>
-                    <p className='text-lg'>{currency}{item.price}</p>
-                    <p>Quantity: 1</p>
-                    <p>Size: Medium</p>
+                    <p>{currency}{item.price}</p>
+                    <p>Quantity: {item.quantity}</p>
+                    <p>Size: {item.size}</p>
                   </div>
-                  <p className='mt-2'>
-                    Date: <span className='text-gray-400'>25, Jul, 2026</span>
+                  <p className='mt-1'>
+                    Date: <span className='text-gray-400'>{new Date(item.date).toDateString()}</span>
+                  </p>
+                  <p className='mt-1'>
+                    Payment: <span className='text-gray-400'>{item.paymentMethod}</span>
                   </p>
                 </div>
               </div>
               <div className='md:w-1/2 flex justify-between'>
                   <div className='flex items-center gap-2'>
                     <p className='min-w-2 rounded-full h-2 bg-green-500'></p>
-                    <p className='text-sm md:text-base'>Ready to ship</p>
+                    <p className='text-sm md:text-base'>{item.status}</p>
                   </div>
-                  <button className='border px-4 py-2 text-sm font-medium rounded-sm'>Track Order</button>
+                  <button onClick={loadOrderData} className='border px-4 py-2 text-sm font-medium rounded-sm'>Track Order</button>
               </div>
             </div>
           ))
